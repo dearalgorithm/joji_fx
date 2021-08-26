9d7786852f0860d5fb647b952810c3730d16a37d
mugiwarafx <mugiwarafx@users.noreply.github.com>
2021-08-26 11:45:41 +0000

    deploy: 3b45157ba85a815f0247c86b4fbb8f9e08f6b79d

--- js/custom_01.js
+++ js/custom_01.js
@@ -1,3 +1,7 @@
-const yourCustomFunction01 = (message) => {
-  console.log(message);
-};
+var links = document.body.getElementsByTagName("a");
+
+for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+   if (links[i].hostname != window.location.hostname) {
+      links[i].target = '_blank';
+   } 
+}
